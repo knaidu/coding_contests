2189e878fb49ff04347318de5383aa0660984522
Karthik Naidu D.J <karthiknaidu.dj@gmail.com>
2012-04-18 07:34:56 -0400

    first

---
+++ insertionSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*Function to sort a collection of integers 
+* Algorithm used - insertion sort
+* Inputs - pointer to an array of positive and negative integers
+*        - size of the input array
+* Output - sorted array
+*/
+int insertionSort(int array[], int n){
+	int array[100];
+	
+	
+}
